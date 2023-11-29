78d3406d524a1ad33636f44293eed16fd1acf4e1
Margo-Katsan <123902159+Margo-Katsan@users.noreply.github.com>
2023-11-29 20:13:49 +0100

    s

--- controllers/auth.js
+++ controllers/auth.js
@@ -45,11 +45,6 @@ const login = async (req, res) => {
     throw HttpError(401, "Email or password invalid");
   }
 
-  if (!user.verify) {
-    throw HttpError(401, "Email not verified")
-    
-  }
-
   const passwordCompare = await bcrypt.compare(password, user.password);
   if (!passwordCompare) {
     throw HttpError(401, "Email or password invalid");
